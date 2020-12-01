33c6eeff1d72ab449fb68f2fc1a031ca361a2e5e
Theo Brigitte <theo.brigitte@gmail.com>
2020-12-01 14:55:41 +0100

    day 1

---
+++ day1/solve2.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func contestResponse() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	data := []int{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		i, err := strconv.Atoi(line)
+		if err != nil {
+			os.Stderr.WriteString(err.Error())
+			return
+		}
+		data = append(data, i)
+	}
+
+	err := scanner.Err()
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+		return
+	}
+
+	result := solve(data)
+	if result == 0 {
+		os.Stderr.WriteString("no results")
+		return
+	}
+
+	fmt.Printf("%d\n", result)
+}
+
+func solve(data []int) int {
+	for _, a := range data {
+		for _, b := range data {
+			for _, c := range data {
+				if a+b+c == 2020 {
+					return a * b * c
+				}
+			}
+		}
+	}
+
+	return 0
+}
+
+func main() {
+	contestResponse()
+}
